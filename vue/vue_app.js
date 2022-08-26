004608d45a2033e78788e746e86f714a94ca9b7b
Matheus Bonavite <matheusbonavite@gmail.com>
2022-08-26 20:30:35 -0300

    adding a content section on the work section

--- vue/vue_app.js
+++ vue/vue_app.js
@@ -36,23 +36,59 @@ App.component('work', {
       companies: [
         {
           name: "Inspr",
-          date: "Oct/2021 (Current)",
-          title: "Software Developer"
+          date: "Oct/2021 - Current Employer",
+          title: "Software Developer",
+          content: [`
+            Hired to work under a 40 hours per week contract.
+            My daily routine consists of adding features, fixing bugs, and talking to the client about the front-end website.
+            Our main client (not named for legal reasons) has a website with over 5000 pages, in a somewhat blog style. For that reason,
+            the Inspr team decided to use <span class="tech">Strapi</span> and <span class="tech">Next.js</span>.
+          `,
+          `
+            Strapi will enable the client to manage the content creation of their own pages in a simplified way;
+            meanwhile Next.js has interesting <span class="tech">Server Side Rendering (SSR)</span> and <span class="tech">Static Site Generation (SSG)</span> features so we can build the whole website
+            before going live (making it super performatic). Even though 5000 pages are a lot of pages to build beforehand, Next.js 12 has enabled <span class="tech">Incremental Static Regeneration (ISR)</span>
+            that help us a lot! Then we can build only about 120 pages beforehand, and the thousands remaining will be built once a first user shows up!
+          `,
+          `
+          Although these do not happen every day, some day-to-day routines also include adding tasks or review tasks on <span class="tech">Jira</span>, also it is common to build images or run them with <span class="tech">Docker</span>
+          (to simulate the production environment locally).
+          `,
+          `Very rarely some code in the back-end application is maintained (usually written in <span class="tech">Go</span> or <span class="tech">TypeScript</span>).`
+        ]
         },
         {
           name: "Embraer",
           date: "May/2021 - Oct/2021",
-          title: "Software Developer Intern"
+          title: "Software Developer Intern",
+          content: [`
+            Hired to work under a 30 hours per week contract. I was hired to help maintain and improve the "CCPM" internal product built at Embraer to organize and schedule the delivery of projects.
+          `, `
+            Most often I spent my days fixing bugs or implementing features on the front-end part of the application, written in plain <span class="tech"> JavaScript </span>, <span class="tech"> HTML </span>, and <span class="tech">CSS</span>.
+          `,
+          `
+            Also improved queries written in the <span class="tech">SQL</span> language (using <span class="tech">MySQL</span> as a DBMS), and from time to time had to write <span class="tech"> PHP </span> code to fix minor bugs in the back-end.
+          `,
+          `
+            Working here was an amazing opportunity, since Embraer stored all its code in local servers, my superiors decided to avoid installing dependencies so all of the "CCPM" code was very
+            back-bones, once packages, libraries, or frameworks (like Angular, React, or Vue) were forbidden. This way, I've learned much about the old fashioned ways of creating websites (not even JQuery was allowed).
+          `]
         },
         {
           name: "SYDLE",
           date: "May/2020 - Abr/2021",
-          title: "Software Developer Intern"
+          title: "Software Developer Intern",
+          content: `
+
+          `
         },
         {
           name: "UFMG",
           date: "Oct/2018 - Dec/2019",
-          title: "Teaching Assistant"
+          title: "Teaching Assistant",
+          content: `
+
+          `
         }
       ]
     }
