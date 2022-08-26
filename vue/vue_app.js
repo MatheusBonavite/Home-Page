af2aa2ea7ce878583de8166f053fdd226f0cf2ab
Matheus Bonavite <matheusbonavite@gmail.com>
2022-08-26 18:18:26 -0300

    adding conditional rendering and company list

--- vue/vue_app.js
+++ vue/vue_app.js
@@ -7,10 +7,15 @@ const App = Vue.createApp({
     },
     methods: {
       updateUnderLayerPosition(num) {
+        if(num == 1)
+          this.selected = "about-me-card"
+        if(num == 2)
+          this.selected = "work"
         this.underLayerPosition = `under-layer pos-${num}`
       }
     }
 })
+
 App.component('sidebar-button', {
     template: "#sidebar-button",
     props: [],
@@ -21,6 +26,37 @@ App.component('about-me-card', {
   template: "#about-me-card",
   props: [],
   data(){}
+})
+
+App.component('work', {
+  template: "#work",
+  props: [],
+  data(){
+    return {
+      companies: [
+        {
+          name: "Inspr",
+          date: "Oct/2021",
+          title: "Software Developer"
+        },
+        {
+          name: "Embraer",
+          date: "May/2021 - Oct/2021",
+          title: "Software Developer Intern"
+        },
+        {
+          name: "SYDLE",
+          date: "May/2020 - Abr/2021",
+          title: "Software Developer Intern"
+        },
+        {
+          name: "UFMG",
+          date: "Oct/2018 - Dec/2019",
+          title: "Teaching Assistant"
+        }
+      ]
+    }
+  }
 })
 
 App.component('my-git', {
