6a9e9d8dbdcbff2467576ec135be9ddf6f5aca11
Matheus Bonavite <matheusbonavite@gmail.com>
2022-08-26 08:08:56 -0300

    adding some assets and creating info card

--- vue/vue_app.js
+++ vue/vue_app.js
@@ -1,7 +1,8 @@
 const App = Vue.createApp({
     data() {
       return {
-        underLayerPosition: "under-layer pos-1"
+        underLayerPosition: "under-layer pos-1",
+        selected: "about-me-card"
       };
     },
     methods: {
@@ -15,4 +16,11 @@ App.component('sidebar-button', {
     props: [],
     data(){}
 })
+
+App.component('about-me-card', {
+  template: "#about-me-card",
+  props: [],
+  data(){}
+})
+
 App.mount("#app");
